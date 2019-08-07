9063c230eb0dfd72b8265338fb88b530e3facd75
dubigrasu <dubigrasu@yahoo.com>
2019-08-07 18:42:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -71,6 +71,7 @@ fi
 # chroot into target
 pacstrap ${MOUNT_PATH} base
 arch-chroot ${MOUNT_PATH} /bin/bash <<EOF
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
